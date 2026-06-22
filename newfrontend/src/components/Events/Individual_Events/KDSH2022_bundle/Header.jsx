4d2871407b743de668e95b9ff5da4cdd9a7aa4c8
bothrayash276 <bothrayash276@gmail.com>
2026-06-22 20:39:10 +0530

    tailwindcss important syntax fixed

--- newfrontend/src/components/Events/Individual_Events/KDSH2022_bundle/Header.jsx
+++ newfrontend/src/components/Events/Individual_Events/KDSH2022_bundle/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="h-[30rem] !pt-[20rem] bg-cover shadow-[0_2px_10px_rgba(0,0,0,0.25)]">
         <div class="resources-list-header-title">Kharagpur <span className="bolding"> Data Science</span> Hackathon 2022</div>
       </div>
-      <section className="relative flex !-mt-[150px] h-[500px] bg-[rgba(0,0,0,0.7)] justify-center !mb-[20px] max-[480px]:flex-col max-[480px]:h-[300px] max-[480px]:!mt-0">
+      <section className="relative flex -mt-[150px]! h-[500px] bg-[rgba(0,0,0,0.7)] justify-center !mb-[20px] max-[480px]:flex-col max-[480px]:h-[300px] max-[480px]:mt-0!">
         <a href="https://unstop.com/hackathon/kharagpur-data-science-hackathon-2022-indian-institute-of-technology-iit-kharagpur-542463" 
         target="_blank" 
         className="!pr-[100px]">
@@ -24,7 +24,7 @@ const Header = () => {
             className="text-[30px] font-bold !text-white no-underline uppercase !py-[20px] max-[480px]:!py-[20px] max-[480px]:!px-[60px] !px-[60px] relative duration-200 scale-[2] rounded-[10px] w-auto h-auto top-[10px]hover:shadow-[0_0_5px_#ff7272,inset_0_0_5px_#ff7272] delay-200"
             style={{fontFamily : 'consolas'}}>
               <span
-              className="relative z-0 !text-white top-[10px]">
+              className="relative z-0 text-white! top-[10px]">
                 Register
               </span>
               <div 
