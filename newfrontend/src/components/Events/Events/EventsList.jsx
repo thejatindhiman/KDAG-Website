0676ee46eb17c4f6cfe9bf875f24bce57cd24c65
bothrayash276 <bothrayash276@gmail.com>
2026-06-22 08:16:49 +0530

    events folder under events converted

---
+++ newfrontend/src/components/Events/Events/EventsList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import EventsCard from "./EventsCard.jsx";
+import events from "./EventsStatic";
+
+const dummy = [0, 1, 2, 3, 4, 5, 6, 7];
+
+const EventsList = () => {
+  // To be used later
+  // const [blogs, setBlogs] = useState();
+
+  return (
+    <>
+      {events?.map((event) => {
+        return <EventsCard key={event.id} event={event} />;
+      }) ||
+        dummy.map((id) => {
+          return <EventsCard key={id} />;
+        })}
+    </>
+  );
+};
+
+export default EventsList;
