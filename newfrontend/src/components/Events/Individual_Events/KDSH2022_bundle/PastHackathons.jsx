2bd6c248edd3c92f86728474a3e9cf3dddba291f
bothrayash276 <bothrayash276@gmail.com>
2026-06-22 21:56:27 +0530

    past hackathon page converted

--- newfrontend/src/components/Events/Individual_Events/KDSH2022_bundle/PastHackathons.jsx
+++ newfrontend/src/components/Events/Individual_Events/KDSH2022_bundle/PastHackathons.jsx
@@ -36,20 +36,21 @@ const PastHackathons = () => {
           </Fade>
           <Fade right>
             <a className="w-[50%] p-[2rem]! max-[800px]:w-full" href="https://unstop.com/p/kharagpur-data-science-hackathon-indian-institute-of-technology-iit-kharagpur-232484" target="_blank" rel="noopener noreferrer">
-              <img src={KDSH2021} alt="CONTENT GRAPHICS 1" />
+              <img src={KDSH2021} alt="CONTENT GRAPHICS 1" className="w-full" />
             </a>
           </Fade>
         </div>
 
-        <div className="past-pair">
+        <div className="flex max-[800px]:block">
           <Fade left>
-            <div className="past-pair-text past-mobile">
-              <div className="past-pair-heading">
-                <span className="past-pair-word">
+            <div className="w-[50%] text-[1.2rem] p-[2rem]! max-[800px]:w-full hidden max-[800px]:block">
+              <div className="font-[2.5rem] font-bold mb-[1rem]! text-[#d9dddc]!">
+                <span className="uppercase">
                   Kharagpur Data Science Hackathon, 1st Edition
                 </span>
               </div>
-              <div className="past-pair-paragraph">
+              <div className="text-[#a5a9ae]!"
+              style={{fontFamily : '"Merriweather", "Times New Roman", serif'}}>
                 Date: 15th March, 2021 - 1st April, 2021 <br /> <br />
                 Sponsored by: Airtel <br /> <br />
                 Prize Money: Rs 50,000
@@ -59,8 +60,8 @@ const PastHackathons = () => {
                 target="_blank"
                 rel="noreferrer noopener"
               >
-                <div className="past-card-button">
-                  <div className="past-card-button2">
+                <div className="w-[60%] m-auto! bg-[#4e3eff] text-white! flex shadow-[1px_1px_10px_1px_rgba(0,0,0,0.5)] min-w-[20rem] mt-[2.5rem]! hover:bg-[#7f74fb]">
+                  <div className="inline-block p-[1rem]! grow">
                     Event Information
                   </div>
                 </div>
@@ -68,23 +69,24 @@ const PastHackathons = () => {
             </div>
           </Fade>
           <Fade right>
-            <a className="past-pair-graphics past-mobile" href="https://unstop.com/hackathon/kharagpur-data-science-hackathon-kharagpur-data-analytics-group-148743?fbclid=IwAR1E1C-_-4gCSdY8FkZ4eaa9wk5FbigCm_JDTDoLn10MNIBikDYso2r1p70" target="_blank" rel="noopener noreferrer">
-              <img src={KDSH2020} alt="CONTENT GRAPHICS 1" />
+            <a className="w-[50%] p-[2rem]! max-[800px]:w-full hidden max-[800px]:block" href="https://unstop.com/hackathon/kharagpur-data-science-hackathon-kharagpur-data-analytics-group-148743?fbclid=IwAR1E1C-_-4gCSdY8FkZ4eaa9wk5FbigCm_JDTDoLn10MNIBikDYso2r1p70" target="_blank" rel="noopener noreferrer">
+              <img src={KDSH2020} alt="CONTENT GRAPHICS 1" className="w-full" />
             </a>
           </Fade>
           <Fade left>
-            <a className="past-pair-graphics past-nonmobile" href="https://unstop.com/hackathon/kharagpur-data-science-hackathon-kharagpur-data-analytics-group-148743?fbclid=IwAR1E1C-_-4gCSdY8FkZ4eaa9wk5FbigCm_JDTDoLn10MNIBikDYso2r1p70" target="_blank" rel="noopener noreferrer">
-              <img src={KDSH2020} alt="CONTENT GRAPHICS 1" />
+            <a className="w-[50%] p-[2rem]! max-[800px]:w-full past-nonmobile" href="https://unstop.com/hackathon/kharagpur-data-science-hackathon-kharagpur-data-analytics-group-148743?fbclid=IwAR1E1C-_-4gCSdY8FkZ4eaa9wk5FbigCm_JDTDoLn10MNIBikDYso2r1p70" target="_blank" rel="noopener noreferrer">
+              <img src={KDSH2020} alt="CONTENT GRAPHICS 1" className="w-full" />
             </a>
           </Fade>
           <Fade right>
-            <div className="past-pair-text past-nonmobile">
-              <div className="past-pair-heading past-nonmobile">
-                <span className="past-pair-word">
+            <div className="w-[50%] text-[1.2rem] p-[2rem]! max-[800px]:w-full hidden max-[800px]:block past-nonmobile">
+              <div className="font-[2.5rem] font-bold mb-[1rem]! text-[#d9dddc]! past-nonmobile">
+                <span className="uppercase">
                   Kharagpur Data Science Hackathon, 1st Edition
                 </span>
               </div>
-              <div className="past-pair-paragraph past-nonmobile">
+              <div className="text-[#a5a9ae] past-nonmobile"
+              style={{fontFamily : '"Merriweather", "Times New Roman", serif'}}>
                 Date: 15th March, 2021 - 1st April, 2021 <br /> <br />
                 Sponsored by: Airtel <br /> <br />
                 Prize Money: Rs 50,000
@@ -94,8 +96,8 @@ const PastHackathons = () => {
                 target="_blank"
                 rel="noreferrer noopener"
               >
-                <div className="past-card-button">
-                  <div className="past-card-button2">
+                <div className="w-[60%] m-auto! bg-[#4e3eff] text-white! flex shadow-[1px_1px_10px_1px_rgba(0,0,0,0.5)] min-w-[20rem] mt-[2.5rem]! hover:bg-[#7f74fb]">
+                  <div className="inline-block p-[1rem]! grow">
                     Event Information
                   </div>
                 </div>
