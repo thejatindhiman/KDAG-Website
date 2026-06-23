453a14da9a20514cc3d3601687189b8c0bafe3e8
bothrayash276 <bothrayash276@gmail.com>
2026-06-23 16:55:27 +0530

    certificate star guide converted

---
+++ newfrontend/src/components/CertificateGeneration/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div class="resources-list-header">
+            <div class="resources-list-header-title">
+                CERTIFICATE GENERATION
+            </div>
+        </div>
+    );
+};
+
+export default Header;
